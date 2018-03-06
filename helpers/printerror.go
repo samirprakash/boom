5528f3699948596cac23d306050c9ccba5b0ee69
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-03-06 12:36:53 -0600

    Extracted fucntions to helper pkg

--- helpers/printerror.go
+++ helpers/printerror.go
@@ -1,1 +1,13 @@
 package helpers
+
+import (
+	"fmt"
+	"os"
+)
+
+//PrintError prints the errors generated from the CLI execution, if any
+func PrintError(err error) {
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("==> Error : %s\n", err.Error()))
+	}
+}
--- helpers/printoutput.go
+++ helpers/printoutput.go
@@ -1,1 +1,10 @@
 package helpers
+
+import "fmt"
+
+// PrintOutput prints the output of CLI execution
+func PrintOutput(outs []byte) {
+	if len(outs) > 0 {
+		fmt.Printf("==> Output : %s\n", string(outs))
+	}
+}
