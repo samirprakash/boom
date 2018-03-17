5520ba334a861ace368bd95b3fc85839bf7d72af
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-03-16 21:47:20 -0500

    Formatted command output

--- helpers/printoutput.go
+++ helpers/printoutput.go
@@ -5,6 +5,6 @@ import "fmt"
 // PrintOutput prints the output of CLI execution
 func PrintOutput(outs []byte) {
 	if len(outs) > 0 {
-		fmt.Printf("==> Output : %s\n", string(outs))
+		fmt.Printf("==> Output : \n%s\n", string(outs))
 	}
 }
