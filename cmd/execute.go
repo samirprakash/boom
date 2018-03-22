7737b261a81bc2518590e880579300391847be25
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-03-22 18:33:54 -0500

    Renamed vars for clarity

--- cmd/execute.go
+++ cmd/execute.go
@@ -9,9 +9,9 @@ import (
 func execute(c string) {
 	tokens := strings.Fields(c)
 	executable := tokens[0]
-	tokens = tokens[1:len(tokens)]
+	args := tokens[1:len(tokens)]
 
-	cmd := exec.Command(executable, tokens...)
+	cmd := exec.Command(executable, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
