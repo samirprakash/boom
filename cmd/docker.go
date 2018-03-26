aa885239a3073bf65ae97824585211937495002c
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-03-25 21:56:56 -0500

    Read ports and compose file from user input

--- cmd/docker.go
+++ cmd/docker.go
@@ -8,9 +8,11 @@ import (
 )
 
 var (
-	imageTag   string
-	appType    string
-	uploadPath string
+	imageTag         string
+	appType          string
+	uploadPath       string
+	composeFile      string
+	healthcheckPorts string
 
 	// dockerCmd is the parent command to execute docker and docker-compose actions
 	// execute go-boom docker -h to check the available options
@@ -27,7 +29,7 @@ Prerequisites:
 
 Example usage options:
 	- go-boom docker [ build | compose | run | tag ] -h
-	- go-boom docker build [ --image-tag | -i ] [[ --app-type | -t ] -h
+	- go-boom docker build [ --image-tag | -i ] [ --app-type | -t ] -h
 	- go-boom docker compose [ --compose-file | -f ] -h
 	- go-boom docker run [ --network-bridge | -n ] [ --integration-test-file | -f ] [ --environment-file | -e ] -h
 	- go-boom docker tag [ --current-image | -i ] [ --new-image | -n ] -h
@@ -40,7 +42,7 @@ Example usage options:
 	imageCmd = &cobra.Command{
 		Use:     "build",
 		Short:   "Build docker images and push to a remote repository",
-		Example: "go-boom docker build [ --image-tag | -i ] [[ --app-type | -t ] -h",
+		Example: "go-boom docker build [ --image-tag | -i ] [ --app-type | -t ] -h",
 		Run: func(cmd *cobra.Command, args []string) {
 			if uploadPath == "" {
 				fmt.Fprintln(os.Stderr, "\nMissing data - please provide the path to your docker registry. \nRun `go-boom docker build -h` for usage guidelines!")
@@ -59,13 +61,39 @@ Example usage options:
 			execute(pushImage)
 		},
 	}
+
+	// compose is the subcommand to start a docker compose environment to integration testing
+	// execute go-boom docker compose -h to check the available options
+	composeCmd = &cobra.Command{
+		Use:     "compose",
+		Short:   "Create docker compose environment based on the docker-compose.yaml in the code base",
+		Example: "go-boom docker compose [ --compose-file | -f ] [ --healthcheck-ports | -p ] -h",
+		Run: func(cmd *cobra.Command, args []string) {
+			if composeFile == "" {
+				fmt.Fprintln(os.Stderr, "\nMissing data - please provide the docker compose file. \nRun `go-boom docker compose -h` for usage guidelines!")
+				return
+			} else if healthcheckPorts == "" {
+				fmt.Fprintln(os.Stderr, "\nMissing data - please provide the healthcheck ports exposed in the docker compose file. \nRun `go-boom docker compose -h` for usage guidelines!")
+				return
+			}
+			fmt.Println(composeFile)
+			fmt.Println(healthcheckPorts)
+			// buildImage := "docker build --tag " + tag + " ."
+			// pushImage := "docker push " + tag
+			// execute(buildImage)
+			// execute(pushImage)
+		},
+	}
 )
 
 func init() {
 	imageCmd.Flags().StringVarP(&uploadPath, "upload-to", "u", "", "specify the url to your docker registry")
 	imageCmd.Flags().StringVarP(&imageTag, "image-tag", "i", "", "specify the tag for your image")
 	imageCmd.Flags().StringVarP(&appType, "app-type", "t", "", "specifcy the application type - services/client")
+	composeCmd.Flags().StringVarP(&composeFile, "compose-file", "f", "", "specify the compose file to used for setting up the environment")
+	composeCmd.Flags().StringVarP(&healthcheckPorts, "healthcheck-ports", "p", "", "specify the healthcheck ports exposed in the compose file - use comma seperated format")
 
 	rootCmd.AddCommand(dockerCmd)
 	dockerCmd.AddCommand(imageCmd)
+	dockerCmd.AddCommand(composeCmd)
 }
