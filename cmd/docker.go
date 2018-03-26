69ecc90878f352c0d88fbe7c4e0272a941e15211
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-03-25 21:31:23 -0500

    Added builder and push command for docker

--- cmd/docker.go
+++ cmd/docker.go
@@ -1,10 +1,16 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
 var (
+	imageTag   string
+	appType    string
+	uploadPath string
 
 	// dockerCmd is the parent command to execute docker and docker-compose actions
 	// execute go-boom docker -h to check the available options
@@ -21,15 +27,44 @@ Prerequisites:
 
 Example usage options:
 	- go-boom docker [ build | compose | run | tag ] -h
-	- go-boom docker build [ --image-tag | -i ] -h
+	- go-boom docker build [ --image-tag | -i ] [[ --app-type | -t ] -h
 	- go-boom docker compose [ --compose-file | -f ] -h
 	- go-boom docker run [ --network-bridge | -n ] [ --integration-test-file | -f ] [ --environment-file | -e ] -h
 	- go-boom docker tag [ --current-image | -i ] [ --new-image | -n ] -h
 		`,
 		Args: cobra.MinimumNArgs(1),
 	}
+
+	// buildCmd is the subcommand to generate docker images
+	// execute go-boom docker build -h to check the available options
+	builderCmd = &cobra.Command{
+		Use:     "build",
+		Short:   "Build docker images for packaing your code base",
+		Example: "go-boom docker build [ --image-tag | -i ] [[ --app-type | -t ] -h",
+		Run: func(cmd *cobra.Command, args []string) {
+			if uploadPath == "" {
+				fmt.Fprintln(os.Stderr, "Missing data - please provide the path to your docker registry. \n\nRun `go-boom docker build -h` for usage guidelines!")
+				return
+			} else if imageTag == "" {
+				fmt.Fprintln(os.Stderr, "Missing data - please provide the image tag. \n\nRun `go-boom docker build -h` for usage guidelines!")
+				return
+			} else if appType == "" {
+				fmt.Fprintln(os.Stderr, "Missing data - please provide the application type. \n\nRun `go-boom docker build -h` for usage guidelines!")
+				return
+			}
+			tag := uploadPath + "/" + appType + "/" + imageTag
+			c := "docker build --tag " + tag + " . && docker push " + tag
+			fmt.Println(c)
+			execute(c)
+		},
+	}
 )
 
 func init() {
+	builderCmd.Flags().StringVarP(&uploadPath, "upload-to", "u", "", "specify the url to your docker registry")
+	builderCmd.Flags().StringVarP(&imageTag, "image-tag", "i", "", "specify the tag for your image")
+	builderCmd.Flags().StringVarP(&appType, "app-type", "t", "", "specifcy the application type - services/client")
+
 	rootCmd.AddCommand(dockerCmd)
+	dockerCmd.AddCommand(builderCmd)
 }
