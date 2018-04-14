a3e228d74fe3533b1239728ea25cd4532d98bd27
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-04-14 18:04:01 -0500

    Format info handler

---
+++ pkg/check/info.go
@@ -0,0 +1,9 @@
+package check
+
+import "fmt"
+
+// IfInfo should be used to describe the example commands that are about to run
+// or to display any information on the console.
+func IfInfo(format string, args ...interface{}) {
+	fmt.Printf("\x1b[34;1m%s\x1b[0m\n", fmt.Sprintf(format, args...))
+}
