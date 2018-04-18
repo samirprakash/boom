21735aae55a499b7c791b985d6ca0a9ece1f5dfa
Samir Prakash <Samir.Prakash@toyotaconnected.com>
2018-04-18 16:37:51 -0500

    Removed custom logging pkg

--- pkg/handle/info.go
+++
@@ -1,9 +0,0 @@
-package handle
-
-import "fmt"
-
-// IfInfo should be used to describe the example commands that are about to run
-// or to display any information on the console.
-func Info(format string, args ...interface{}) {
-	fmt.Printf("\x1b[34;1m%s\x1b[0m\n", fmt.Sprintf(format, args...))
-}
